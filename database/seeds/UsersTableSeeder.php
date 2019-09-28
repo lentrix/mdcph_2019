d51dade576ca1e517c379a0a4aa000981ff1d888
lentrix <hawkmanlentrix@gmail.com>
2019-09-28 19:45:02 +0800

    initial

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\User::create([
+            'name' => "Benjie B. Lenteria",
+            'username' => 'lentrix',
+            'password' => bcrypt('system'),
+            'role' => 'admin'
+        ]);
+    }
+}
---
+++ resources/views/main.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <title>{{env('APP_NAME')}}</title>
+</head>
+<body>
+
+    <header>
+        <div class="container">
+            <a href="{{url('/')}}">
+                <img src="{{asset('images/MDC-Logo-clipped.png')}}" alt="MDC Logo">
+                <span class="title">Mater Dei College</span>
+            </a>
+        </div>
+    </header>
+    <div class="jumbotron">
+    <div class="container">
+            <h1>We are rebuilding!</h1>
+            <p>Were are currently rebuilding our website. Please check back soon!</p>
+        </div>
+    </div>
+
+</body>
+</html>
